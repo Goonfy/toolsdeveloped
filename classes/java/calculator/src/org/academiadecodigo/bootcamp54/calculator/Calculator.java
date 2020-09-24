0a834e8346de154705f09d8382bd38b181a8e617
ThaKushi <edupedro1996@gmail.com>
2020-09-24 17:19:07 +0100

    add divide, subtract, multiply methods to calculator

--- classes/java/calculator/src/org/academiadecodigo/bootcamp54/calculator/Calculator.java
+++ classes/java/calculator/src/org/academiadecodigo/bootcamp54/calculator/Calculator.java
@@ -11,4 +11,13 @@ public class Calculator {
 
     public int add(int n1, int n2) { return n1 + n2; }
     public float add(float n1, float n2) { return n1 + n2; }
+
+    public int divide(int n1, int n2) { return n1 / n2; }
+    public float divide(float n1, float n2) { return n1 / n2; }
+
+    public int subtract(int n1, int n2) { return n1 - n2; }
+    public float subtract(float n1, float n2) { return n1 - n2; }
+
+    public int multiply(int n1, int n2) { return n1 * n2; }
+    public float multiply(float n1, float n2) { return n1 * n2; }
 }
--- classes/java/calculator/src/org/academiadecodigo/bootcamp54/calculator/Main.java
+++ classes/java/calculator/src/org/academiadecodigo/bootcamp54/calculator/Main.java
@@ -5,8 +5,17 @@ public class Main {
         Calculator chinese = new Calculator("gray", "tlintaetles");
         Calculator spanish = new Calculator("amarilho", "cucaracha");
 
-        System.out.println(chinese.add(1, 1));
-        System.out.println(spanish.add(1.4f, 2.5f));
+        System.out.println("Sum: " + chinese.add(1, 1));
+        System.out.println("Sum float: " + spanish.add(1.4f, 2.5f));
+
+        System.out.println("Divide: " + chinese.divide(1, 1));
+        System.out.println("Divide float: " + spanish.divide(1.4f, 2.5f));
+
+        System.out.println("Subtract: " + chinese.subtract(1, 1));
+        System.out.println("Subtract float: " + spanish.subtract(1.4f, 2.5f));
+
+        System.out.println("Multiply: " + chinese.multiply(1, 1));
+        System.out.println("Multiply float: " + spanish.multiply(1.4f, 2.5f));
 
         System.out.println("Brand: " + spanish.brand + " Color: " + spanish.color);
         System.out.println("Brand: " + chinese.brand + " Color: " + chinese.color);
