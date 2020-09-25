dcf622af9b8e449c30d68aa54b3c654d3e22aeb4
ThaKushi <edupedro1996@gmail.com>
2020-09-25 18:26:14 +0100

    tweaked calculator

--- classes/java/calculator/src/org/academiadecodigo/bootcamp54/calculator/Main.java
+++ classes/java/calculator/src/org/academiadecodigo/bootcamp54/calculator/Main.java
@@ -2,22 +2,20 @@ package org.academiadecodigo.bootcamp54.calculator;
 
 public class Main {
     public static void main(String[] args) {
-        Calculator chinese = new Calculator("gray", "tlintaetles");
-        Calculator spanish = new Calculator("amarilho", "cucaracha");
+        Calculator calculator = new Calculator("gray", "tlintaetles");
 
-        System.out.println("Sum: " + chinese.add(1, 1));
-        System.out.println("Sum float: " + spanish.add(1.4f, 2.5f));
+        System.out.println("Sum: " + calculator.add(1, 1));
+        System.out.println("Sum float: " + calculator.add(1.4f, 2.5f));
 
-        System.out.println("Divide: " + chinese.divide(1, 1));
-        System.out.println("Divide float: " + spanish.divide(1.4f, 2.5f));
+        System.out.println("Divide: " + calculator.divide(1, 1));
+        System.out.println("Divide float: " + calculator.divide(1.4f, 2.5f));
 
-        System.out.println("Subtract: " + chinese.subtract(1, 1));
-        System.out.println("Subtract float: " + spanish.subtract(1.4f, 2.5f));
+        System.out.println("Subtract: " + calculator.subtract(1, 1));
+        System.out.println("Subtract float: " + calculator.subtract(1.4f, 2.5f));
 
-        System.out.println("Multiply: " + chinese.multiply(1, 1));
-        System.out.println("Multiply float: " + spanish.multiply(1.4f, 2.5f));
+        System.out.println("Multiply: " + calculator.multiply(1, 1));
+        System.out.println("Multiply float: " + calculator.multiply(1.4f, 2.5f));
 
-        System.out.println("Brand: " + spanish.brand + " Color: " + spanish.color);
-        System.out.println("Brand: " + chinese.brand + " Color: " + chinese.color);
+        System.out.println("Brand: " + calculator.brand + " Color: " + calculator.color);
     }
 }
