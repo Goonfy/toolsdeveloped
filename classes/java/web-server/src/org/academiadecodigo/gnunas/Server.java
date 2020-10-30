6c13a7978eb88be696fabd5aebca3f1ca0636555
ThaKushi <edupedro1996@gmail.com>
2020-10-30 00:38:58 +0000

    finished exercise web server, still needs tweaking and reorganization

--- classes/java/web-server/src/org/academiadecodigo/gnunas/Server.java
+++ classes/java/web-server/src/org/academiadecodigo/gnunas/Server.java
@@ -1,5 +1,7 @@
 package org.academiadecodigo.gnunas;
 
+import org.academiadecodigo.gnunas.handler.HeaderType;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -44,22 +46,24 @@ public class Server {
 
             File file = new File("www" + path);
 
-            byte[] header = ("HTTP/1.0 200 Document Follows\r\n" +
-                    "Content-Type: text/html; charset=UTF-8\r\n" +
-                    "Content-Length: " + file.length() + "\r\n" +
-                    "\r\n").getBytes();
+            byte[] header = null;
+            if (!file.exists() || !path.contains(".")) {
+                file = new File("www/404.html");
+                header = HeaderType.NOTFOUND.setHeader(file);
+            }
+            else if (path.contains(".png") || path.contains(".jpg") || path.contains(".gif")) {
+                header = HeaderType.IMAGE.setHeader(file);
+            }
+            else if (path.contains(".html")) {
+                header = HeaderType.HTML.setHeader(file);
+            }
 
-            sendPacket(header, file);
+            if (header != null) {
+                sendPacket(header, file);
+            }
+
+            server();
         }
-
-        server();
-    }
-
-    private byte[] fileToBytes(File file) throws IOException {
-        byte[] bufferReader = new byte[1024];
-        FileInputStream fileReader = new FileInputStream(file);
-        fileReader.read(bufferReader);
-        return bufferReader;
     }
 
     private BufferedReader receivePacket() throws IOException {
@@ -68,8 +72,14 @@ public class Server {
 
     private void sendPacket(byte[] header, File file) throws IOException {
         DataOutputStream dataOutputStream = new DataOutputStream(clientSocket.getOutputStream());
+        FileInputStream fileInputStream = new FileInputStream(file);
+
         dataOutputStream.write(header);
-        dataOutputStream.write(fileToBytes(file));
-        dataOutputStream.flush();
+
+        byte[] buffer = new byte[1024];
+        while (fileInputStream.available() != 0) {
+            int readBytes = fileInputStream.read(buffer);
+            dataOutputStream.write(buffer, 0, readBytes);
+        }
     }
 }
--- classes/java/web-server/src/org/academiadecodigo/gnunas/handler/HeaderType.java
+++ classes/java/web-server/src/org/academiadecodigo/gnunas/handler/HeaderType.java
@@ -1,4 +1,24 @@
 package org.academiadecodigo.gnunas.handler;
 
+import java.io.File;
+
 public enum HeaderType {
+    HTML("200 Document Follows", "text/html"),
+    IMAGE("200 Document Follows", "image/gif"),
+    NOTFOUND("404 Not Found", "text/html");
+
+    private final String code;
+    private final String contentType;
+
+    HeaderType(String code, String contentType) {
+        this.code = code;
+        this.contentType = contentType;
+    }
+
+    public byte[] setHeader(File file) {
+        return ("HTTP/1.0 " + code + "\r\n" +
+                "Content-Type: " + contentType + "; charset=UTF-8\r\n" +
+                "Content-Length: " + file.length() + "\r\n" +
+                "\r\n").getBytes();
+    }
 }
