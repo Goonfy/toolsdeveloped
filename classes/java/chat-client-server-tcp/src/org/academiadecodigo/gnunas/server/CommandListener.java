98710286ba3e9fb3731c9ac6e38408fa2fc06a3b
ThaKushi <edupedro1996@gmail.com>
2020-11-05 16:08:11 +0000

    Added some methods to commandlistener class

--- classes/java/chat-client-server-tcp/src/org/academiadecodigo/gnunas/server/CommandListener.java
+++ classes/java/chat-client-server-tcp/src/org/academiadecodigo/gnunas/server/CommandListener.java
@@ -13,70 +13,79 @@ public class CommandListener {
 
     protected boolean check(String username, String message) throws IOException {
         if (message.contains("/kick")) {
-            kick(message);
-            return true;
+            return kick(message);
         }
 
         return false;
     }
 
-    protected void kick(String message) throws IOException {
+    protected boolean kick(String message) throws IOException {
         String[] m = message.split(" ");
         if (m.length < 2) {
-            return;
-        }
-
-        String toUser = m[1];
-        Socket userSocket = server.getUser(toUser).getClientSocket();
-        if (userSocket == null) {
-            return;
-        }
-
-        server.sendMessagePacketToAll(toUser + " has been kicked!!");
-        server.disconnect(userSocket);
-    }
-
-    protected void logout(String username) throws IOException {
-        server.disconnect(server.getUser(username).getClientSocket());
-    }
-
-    protected void users(String username) throws IOException {
-        if (server.getUser(username) == null) {
-            return;
-        }
-
-        server.sendMessagePacketTo(server.getUser(username).getClientSocket(), "Online users: " + server.getConnectedClients().toString());
-    }
-
-    protected void name(String message) throws IOException {
-        String[] m = message.split(" ");
-        if (m.length < 2) {
-            return;
+            return false;
         }
 
         String toUser = m[1];
         User user = server.getUser(toUser);
         if (user == null) {
-            return;
+            return false;
+        }
+
+        Socket userSocket = user.getClientSocket();
+
+        server.sendMessagePacketToAll(toUser + " has been kicked!!");
+        server.disconnect(userSocket);
+
+        return true;
+    }
+
+    protected boolean logout(String username) throws IOException {
+        server.disconnect(server.getUser(username).getClientSocket());
+        return true;
+    }
+
+    protected boolean users(String username) throws IOException {
+        if (server.getUser(username) == null) {
+            return false;
+        }
+
+        server.sendMessagePacketTo(server.getUser(username).getClientSocket(), "Online users: " + server.getConnectedClients().toString());
+
+        return true;
+    }
+
+    protected boolean name(String message) throws IOException {
+        String[] m = message.split(" ");
+        if (m.length < 2) {
+            return false;
+        }
+
+        String toUser = m[1];
+        User user = server.getUser(toUser);
+        if (user == null) {
+            return false;
         }
 
         server.sendMessagePacketToAll(user + " changed username to " + toUser);
         user.setUsername(toUser);
+
+        return true;
     }
 
-    protected void pm(String username, String message) throws IOException {
+    protected boolean pm(String username, String message) throws IOException {
         String[] m = message.split(" ");
-        if (m.length < 2) {
-            return;
+        if (m.length < 3) {
+            return false;
         }
 
         String toUser = m[1];
         Socket userSocket = server.getUser(toUser).getClientSocket();
         if (userSocket == null) {
-            return;
+            return false;
         }
 
-        server.sendMessagePacketTo(userSocket, "[PM] " + message);
+        server.sendMessagePacketTo(userSocket, "[PM] " + m[2]);
 
+        return true;
     }
 }
